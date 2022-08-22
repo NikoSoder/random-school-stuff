07ee8e45c57cb8bb80f8401d3a5ea69624934b36
Niko <soder.n@gmail.com>
2022-08-22 14:49:26 +0300

    5.13

---
+++ 5.13/Item.cs
@@ -0,0 +1,36 @@
+namespace Exercise013
+{
+    public class Item
+    {
+
+        private string identifier;
+        private string name;
+
+        public Item(string identifier, string name)
+        {
+            this.identifier = identifier;
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return this.identifier + ": " + this.name;
+        }
+
+        public override bool Equals(object compared)
+        {
+            //compare with Item.identifier
+            if (!(compared is Item)) return false;
+            if (this == compared) return true;
+            if(compared == null) return false;
+            Item comparedItem = (Item)compared;
+            if (this.identifier == comparedItem.identifier)
+            {
+                return true;
+            }
+            return false;
+
+            
+        }
+    }
+
+}
---
+++ 5.13/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Exercise013
+{
+    using System;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+
+            // Ask for input as shown in the exercise.
+            while (true)
+            {
+                Console.WriteLine("Identifier? (empty will stop):");
+                string identifierInput = Console.ReadLine();
+                if (identifierInput == "")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Name? (empty will stop):");
+                string name = Console.ReadLine();
+                Item item = new Item(identifierInput, name);
+
+                if(items.Contains(item))
+                {
+                    continue;
+                }
+                else 
+                {
+                    items.Add(item);
+                }
+
+            }
+
+
+            // The end printing is ready, don't touch this
+            Console.WriteLine("==Items==");
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
