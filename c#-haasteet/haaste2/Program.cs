123afcffd0cba211be226288301d8f14886298a5
Niko <soder.n@gmail.com>
2022-08-25 10:35:35 +0300

    haaste2

---
+++ c#-haasteet/haaste2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace haaste2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start() 
+        {
+            // kuinka monta pistettä per tila
+            int pisteetPerMaatila = 1;
+            int pisteetPerHerttuakunta = 3;
+            int pisteetPerMaakunta = 6;
+
+            // kysytään käyttäjän pisteet
+            Console.WriteLine("Montako maatilaa?");
+            int maatilat = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Montako herttuakuntaa?");
+            int herttuakunnat = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Montako maakuntaa?");
+            int maakunnat = Convert.ToInt32(Console.ReadLine());
+
+            // lasketaan pisteet per tila
+            int maatilaPisteet = pisteetPerMaatila * maatilat;
+            int herttuakuntaPisteet = pisteetPerHerttuakunta * herttuakunnat;
+            int maakuntaPisteet = pisteetPerMaakunta * maakunnat;
+
+            // summataan pisteet 
+            int pisteetYhteensä = maatilaPisteet + herttuakuntaPisteet + maakuntaPisteet;
+            Console.WriteLine($"Sait {pisteetYhteensä} pistettä");
+        }
+    }
+}
