92cdfaba4ad97935a8c628343cd257a50eb45899
Niko <soder.n@gmail.com>
2022-08-25 11:25:01 +0300

    haaste6

---
+++ c#-haasteet/haaste6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace haaste6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while(counter < 101) 
+            {
+                if(counter % 3 == 0 && counter % 5 == 0) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{counter}: Electric and Fire");
+                    counter++;
+                }
+                else if(counter % 5 == 0) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{counter}: Electric");
+                    counter++;
+                }
+                else if(counter % 3 == 0) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{counter}: Fire");
+                    counter++;
+                }
+                else 
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"{counter}: Normal");
+                    counter++;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
