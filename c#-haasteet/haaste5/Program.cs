bad8079ef5091b7254799a83067fa3cda7eb690b
Niko <soder.n@gmail.com>
2022-08-30 20:16:26 +0300

    haaste5

---
+++ c#-haasteet/haaste5/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.Write("User 1, enter a number between 0 and 100: ");
+
+int userOneInt;
+while(true) 
+{   string userOneInput = Console.ReadLine();
+    
+    bool success = int.TryParse(userOneInput, out userOneInt);
+    if (success)
+    {
+        Console.WriteLine($"Thank you!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Write a number");
+    }
+}
+Console.Clear();
+
+
+Console.WriteLine("User 2, quess the number.");
+Console.Write("What is your next quess? ");
+//int userTwoInput = Convert.ToInt32(Console.ReadLine());
