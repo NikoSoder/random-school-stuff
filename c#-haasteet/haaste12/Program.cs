8f1913a4692edbf4262d75d62cbbd81bbe1e7947
Niko <soder.n@gmail.com>
2022-09-05 12:45:06 +0300

    haaste12

--- c#-haasteet/haaste12/Program.cs
+++ c#-haasteet/haaste12/Program.cs
@@ -10,47 +10,35 @@ void Start()
         if(option == "q") break;
         if(option == "1") 
         {
-            KaloritJouleiksi();
+            Console.Write("Kirjoita kalorimäärä: ");
+            Laskin(option);
         }
         else if(option == "2") 
         {
-            JouletKaloreiksi();
+            Console.Write("Kirjoita joulimäärä: ");
+            Laskin(option);
         }
-        else
-        {
-            Console.WriteLine("Paina 1, 2 tai q");
-            continue;
-        }
+        else Console.WriteLine("Kirjoita 1, 2 tai q");
     }
-    
 }
 
-void KaloritJouleiksi() 
+void Laskin(string option) 
 {
-    Console.Write("Kirjoita kalorimäärä: ");
-    string kalorit = Console.ReadLine();
-    int kaloritInt;
+    string maara = Console.ReadLine();
+    int maaraInt;
     
-    bool success = int.TryParse(kalorit, out kaloritInt);
+    bool success = int.TryParse(maara, out maaraInt);
     if (success) 
     {
-        Console.WriteLine($"Kalorit jouleiksi: {Math.Round(kaloritInt * 4.184, 2)}");
+        // jos kalorit jouleiksi
+        if(option == "1") 
+        {
+            Console.WriteLine($"Kalorit jouleiksi: {Math.Round(maaraInt * 4.184, 2)}");
+        }
+        // jos joulet kaloreiksi
+        else Console.WriteLine($"Joulet kaloreiksi: {Math.Round(maaraInt * 0.2390, 2)}");  
     }
     else Console.WriteLine("Kirjoita numero");
       
 }
 
-void JouletKaloreiksi() 
-{
-    Console.Write("Kirjoita joulimäärä: ");
-    string joulet = Console.ReadLine();
-    int jouletInt;
-    
-    bool success = int.TryParse(joulet, out jouletInt);
-    if (success) 
-    {
-        Console.WriteLine($"Joulet kaloreiksi: {Math.Round(jouletInt * 0.2390, 2)}");
-    }
-    else Console.WriteLine("Kirjoita numero");
-        
-}
